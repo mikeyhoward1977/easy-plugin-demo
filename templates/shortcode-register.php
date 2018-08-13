65cbb9f78d2bea5f6acc7ee2001d882a3bd9f7ad
mikeyhoward1977 <mike@mikeandniki.co.uk>
2018-08-13 23:30:37 +0100

    Display notices whether logged in or not

--- templates/shortcode-register.php
+++ templates/shortcode-register.php
@@ -4,11 +4,11 @@
  */
 global $epd_register_redirect;
 
-if ( is_user_logged_in() ) :
-	if ( ! empty( $_GET['epd-message'] ) ) :
-		epd_get_template_part( 'register/register', sanitize_text_field( $_GET['epd-message'] ) );
-	endif;
+if ( ! empty( $_GET['epd-message'] ) ) :
+	epd_get_template_part( 'register/register', sanitize_text_field( $_GET['epd-message'] ) );
+endif;
 
+if ( is_user_logged_in() ) :
 	epd_get_template_part( 'register/list', 'sites' );
 endif;
 
