d39c6c23f0120f1bc3469b6d6251ccc832e65be8
Mike Howard <mike@mikeandniki.co.uk>
2018-08-20 18:21:17 +0100

    Add site count to the "Right Now" dashboard widget

--- includes/admin/dashboard/dashboard-actions.php
+++ includes/admin/dashboard/dashboard-actions.php
@@ -33,3 +33,31 @@ function epd_manage_dashboard_welcome_panel()	{
 	}
 } // epd_manage_dashboard_welcome_panel
 add_action( 'wp_dashboard_setup', 'epd_manage_dashboard_welcome_panel' );
+
+/**
+ * Adds the EPD site count to the "Right Now" network dashboard.
+ *
+ * @since   1.0.2
+ * @return  void
+ */
+function epd_right_now_dashboard_site_count()   {
+    $site_count = epd_get_registered_demo_sites_count();
+
+    if ( empty( $site_count ) ) {
+        return;
+    }
+
+    $sites    = sprintf( _n( '%s site', '%s sites', $site_count, 'easy-plugin-demo' ), number_format_i18n( $site_count ) );
+    $sentence = sprintf(
+        __( '%s have been provisioned by <a href="%s" target="_blank">Easy Plugin Demo</a>.', 'easy-plugin-demo' ),
+        $sites,
+        'https://wordpress.org/plugins/easy-plugin-demo/'
+    );
+
+    $settings = add_query_arg( 'page', 'epd-settings', network_admin_url( 'settings' ) );
+
+    ?>
+    <p class="youhave"><?php echo $sentence; ?></p>
+    <?php
+} // epd_right_now_dashboard_site_count
+add_action( 'mu_rightnow_end', 'epd_right_now_dashboard_site_count' );
