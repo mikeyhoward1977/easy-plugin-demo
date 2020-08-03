e22a2f97940e15124e301ea088b275b8a1dbff1e
Mike Howard <mike@the-howards.co.uk>
2020-08-03 14:21:18 +0100

    Remove class from tab

--- includes/admin/settings/display-settings.php
+++ includes/admin/settings/display-settings.php
@@ -111,7 +111,7 @@ function epd_options_page() {
 			}
 
             if ( ! get_site_option( 'epd_premium_version' ) )   {
-                echo '<a href="https://easy-plugin-demo.com/premium-pack/" target="_blank" class="nav-tab' . $active . '">';
+                echo '<a href="https://easy-plugin-demo.com/premium-pack/" target="_blank" class="nav-tab">';
 					_e( 'Go Premium', 'easy-plugin-demo' );
 				echo '</a>';
             }
