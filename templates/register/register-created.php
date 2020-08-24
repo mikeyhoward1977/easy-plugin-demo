ae4ccd6cdbecf1abb3339c717605901c1e9bfdf4
Mike Howard <mike@the-howards.co.uk>
2020-08-24 10:18:01 +0100

    Added the `epd_site_registration_confirmation` filter
    Enables the registration confirmation message that is displayed on the front end to be filtered

--- templates/register/register-created.php
+++ templates/register/register-created.php
@@ -3,6 +3,8 @@
  * This template is used to display a registration confirmation
  */
 $confirmation = __( 'Your {demo_product_name} demo is ready!', 'easy-plugin-demo' );
+
+$confirmation = apply_filters( 'epd_site_registration_confirmation' );
 $confirmation = epd_do_email_tags( $confirmation, $_GET['epd-registered'], get_current_user_id() );
 ?>
 <div class="epd_alert epd_alert_success">
